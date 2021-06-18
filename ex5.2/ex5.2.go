1176dfe1f7061c5d6bf9c78b460a06b04994d522
DESKTOP-076SGDA\cps12 <tmfvmehek@gmail.com>
2021-06-18 16:33:27 +0900

    time 4:33

---
+++ ex5.2/ex5.2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var a int = 10
+	var b int = 20
+	var f float64 = 32799438743.8297
+	fmt.Print("a:", a,"b:",b)
+	fmt.Println("a:", a,"b:",b, "f:", f) // ln은 빈칸이 하나하나에 생긴다
+	fmt.Printf("a:%d b: %d f:%f\n", a,b,f) // %decimal 정수, 위에서는 f가 실수가 지수로 출력이 되었는데 여기서 %f는 실수값으로 표현된다
+	
+}
---
+++ ex5.2/go.mod
@@ -0,0 +1,3 @@
+module goprojects/ex5.2
+
+go 1.16
